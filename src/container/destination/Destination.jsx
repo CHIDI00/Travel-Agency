3cb2becbbcac351d20f449f01104c7cf4ea6553a
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-10-27 18:08:24 +0100

    Fifth Commit

--- src/container/destination/Destination.jsx
+++ src/container/destination/Destination.jsx
@@ -3,26 +3,61 @@ import "./destination.css";
 import destination01 from "../../assets/destination01.jpg";
 import destination02 from "../../assets/destination02.jpg";
 import destination03 from "../../assets/destination03.jpg";
-import sendIcon from "../../assets/sendIcon.png";
+import navigation from "../../assets/navigation.png";
 
 const Destination = function () {
 	return (
-		<div className="taw__destination section__padding">
+		<div className="taw__destination section__padding" id="destination">
 			<div className="taw__destination-container_content">
 				<p>Top selling</p>
 				<h1>Top Destinations</h1>
 			</div>
+
 			<div className="taw__destination-container_card">
-				<div className="taw__destination-card">
-					<img src={destination01} alt="rome" />
+				<div
+					className="taw__destination-card"
+					style={{ backgroundImage: `url('${destination01}')` }}
+				>
 					<div className="taw__destination-card_content">
 						<div className="taw__destination-card_content-location">
 							<p>Rome, Italy</p>
 							<p>$5,42k</p>
 						</div>
 						<div className="taw__destination-card_content-day">
-							<img src={sendIcon} alt="flyIcon" />
-							<p>10 days Trip</p>
+							<img src={navigation} alt="flyIcon" />
+							<p>10 Days Trip</p>
+						</div>
+					</div>
+				</div>
+
+				<div
+					className="taw__destination-card"
+					style={{ backgroundImage: `url('${destination02}')` }}
+				>
+					<div className="taw__destination-card_content">
+						<div className="taw__destination-card_content-location">
+							<p>London, UK</p>
+							<p>$4.2k</p>
+						</div>
+						<div className="taw__destination-card_content-day">
+							<img src={navigation} alt="flyIcon" />
+							<p>12 Days Trip</p>
+						</div>
+					</div>
+				</div>
+
+				<div
+					className="taw__destination-card"
+					style={{ backgroundImage: `url('${destination03}')` }}
+				>
+					<div className="taw__destination-card_content">
+						<div className="taw__destination-card_content-location">
+							<p>Full Europe</p>
+							<p>$15k</p>
+						</div>
+						<div className="taw__destination-card_content-day">
+							<img src={navigation} alt="flyIcon" />
+							<p>28 Days Trip</p>
 						</div>
 					</div>
 				</div>
