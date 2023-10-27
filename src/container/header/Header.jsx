97a0b589a4357932987fc14417c3aa10263bf174
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-10-27 11:32:13 +0100

    Third Commit

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import playBtn from "../../assets/playBtn.jpg";
+import traveller from "../../assets/Traveller 1.png";
+import plane from "../../assets/plane.png";
 import "./header.css";
 
 const Header = function () {
-	return <div>Header</div>;
+	return (
+		<div className="taw__header section__padding">
+			<div className="taw__header-container-left">
+				<h4>Best Destinations around the world</h4>
+				<h1>Travel, enjoy and live a new and full life</h1>
+				<p>
+					Built Wicket longer admire do barton vanity itself do in it. Preferred
+					to sportsmen it engrossed listening. Park gate sell they west hard for
+					the.
+				</p>
+				<div className="taw__header-container_btn">
+					<button type="button">Find out more</button>
+					<img src={playBtn} alt="play" />
+					<p>Play Demo</p>
+				</div>
+			</div>
+
+			<div className="taw__header-image">
+				<img src={plane} alt="plane" />
+				<img src={traveller} alt="traveller" />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
